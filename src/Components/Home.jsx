f2327cdd5e89c8e3c15bc5b5541d055cc4232103
Victor Hernandez <hernandezv0110@gmail.com>
2025-04-23 17:54:49 -0700

    Changed the Site name and picture on the top

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,7 +5,7 @@ import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <div className={styles.Home}>
+    <div id="home" className={styles.Home}>
       <h1 className="white-text">
         Hi, I'm{" "}
         <span className="tech-green">
